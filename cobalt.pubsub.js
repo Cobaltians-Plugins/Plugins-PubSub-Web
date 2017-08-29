d9497bbfb4e4fa8061be53a5cea48f5440d107ea
Guillaume Gendre <dzb.rtz@gmail.com>
2017-08-29 16:17:50 +0200

    add a debugInBrowser fallback

--- cobalt.pubsub.js
+++ cobalt.pubsub.js
@@ -7,11 +7,17 @@
             cobalt.publish=this.publish.bind(this);
             cobalt.subscribe=this.subscribe.bind(this);
             cobalt.unsubscribe=this.unsubscribe.bind(this);
-
         },
         publish:function(channel, message){
             if (typeof channel == "string" && typeof message == "object"){
                 this.send("publish", { channel : channel, message : message });
+
+                if (cobalt.debugInBrowser){
+                  //use a storage fallback for debugging.
+                  console.log('sending cobalt pubsub event for channel', channel, 'with message', message);
+                  localStorage.setItem('pubsub_'+channel, JSON.stringify(message));
+
+                }
             }else{
                 cobalt.log("PubSub : channel must be a string and message should be an object");
             }
@@ -20,6 +26,19 @@
             var callback_id = cobalt.registerCallback(callback)
             if (typeof channel == "string" && typeof callback_id == "string"){
                 this.send("subscribe", { channel : channel, callback : callback_id });
+
+                if (cobalt.debugInBrowser){
+                  //use a storage fallback for debugging.
+                  setInterval(function(){
+                    var msg = localStorage.getItem('pubsub_'+channel);
+                    if (msg) {
+                      var message = JSON.parse(msg);
+                      console.log('received cobalt pubsub event for channel', channel, 'with message', message);
+                      callback(message);
+                      localStorage.removeItem('pubsub_'+channel);
+                    }
+                  },500);
+                }
             }else{
                 cobalt.log("PubSub : channel must be a string and callback should be set");
             }
@@ -35,7 +54,7 @@
             cobalt.log(this.name, ' plugin : unknown event received :', json)
         },
         send:function(action, data, callback){
-			cobalt.send({ type : "plugin", name : this.name, action : action, data : data }, callback);
+            cobalt.send({ type : "plugin", name : this.name, action : action, data : data }, callback);
         }
     };
     cobalt.plugins.register(plugin);
